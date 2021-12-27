aaffd454621fbfaf31177e6c5bef1e183c63f10f
Christiaan Hougaard <cwhougaard@gmail.com>
2021-12-26 23:16:43 -0500

    added nodemailer config and email content. Also updated all secret variables to env.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 9001
 
 const corsOptions = {
-  origin: "*",
+  origin: process.env.ORIGIN_URL,
   methods: ['POST']
 }
 
@@ -32,18 +32,23 @@ app.post('/api/v1/mailer', (req, res)=>{
   });
 
   let mailOptions = {
-    from: name,
+    from: email,
     to: process.env.RECEIVER,
     subject: "Contact Form DevChristiaan",
-    text: (
-      <div>
-        <h1>From: {name}</h1>
-        <h2>Email: {email}</h2>
-        <p>Phone: {phone}</p>
-        <p>Message: {message}</p>
-      </div>
-    )
+    html: `<div>
+        <h2>From: ${name} Email: ${email} Phone: ${phone}</h2>
+        <h4>Message: ${message}</h4>
+      </div>`
   };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log("Error " + err);
+      res.status(417).send("Unable to send message. Please try again later.")
+    } else {
+      res.status(200).send({message: "The cyber monkies delivered your email successfully!", info: data});
+    }
+  });
 })
 
 export default app.listen(PORT, () => console.log(`API server ready on http://localhost:${PORT}`))
